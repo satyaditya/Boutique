7ee8a0b4c5890e5c4e08d34bbd6c8d5b0803b4d3
satyaditya <smunipalle@paypal.com>
2015-02-17 10:20:29 -0800

    added on stop and on pause methods

--- app/src/main/java/com/blueseal/boutique/cart/OrdersFrag.java
+++ app/src/main/java/com/blueseal/boutique/cart/OrdersFrag.java
@@ -6,6 +6,7 @@ package com.blueseal.boutique.cart;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +15,33 @@ import com.blueseal.boutique.R;
 
 public class OrdersFrag extends Fragment
 {
-
+    private final String tag = this.getClass().getCanonicalName();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+        Log.d(tag, "view created");
+        if (savedInstanceState != null) {
+            Log.d(tag, savedInstanceState.toString());
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.orders_frag_layout, container, false);
     }
 
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(tag, "on pause called ");
+
+    }
+
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(tag, "on stop called ");
+
+    }
 }
